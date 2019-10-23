ceb46e0b5f36737f87e9d7f70a456b844bf4c97d
Andre <andrewmarin976@gmail.com>
2019-10-23 12:50:30 +0100

    Updates

---
+++ precache-manifest.f73f77f97a4aaaba56df6c36604bafda.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8546afe0a82b5809647b72ba9787a7",
+    "url": "/erp/index.html"
+  },
+  {
+    "revision": "d19f111a0e2b6373db1d",
+    "url": "/erp/static/css/3.e6157a56.chunk.css"
+  },
+  {
+    "revision": "58325d32d80d1bbee892",
+    "url": "/erp/static/css/5.dff1a273.chunk.css"
+  },
+  {
+    "revision": "bff330fb270340c18896",
+    "url": "/erp/static/css/6.5d7e9f48.chunk.css"
+  },
+  {
+    "revision": "e7f286e4acceb11ed3b9",
+    "url": "/erp/static/css/main.e7f0feae.chunk.css"
+  },
+  {
+    "revision": "6d058995d71fe9d1a752",
+    "url": "/erp/static/js/0.77353096.chunk.js"
+  },
+  {
+    "revision": "d19f111a0e2b6373db1d",
+    "url": "/erp/static/js/3.9484e6a4.chunk.js"
+  },
+  {
+    "revision": "37f47439f85a9b9815cf",
+    "url": "/erp/static/js/4.388b0a77.chunk.js"
+  },
+  {
+    "revision": "58325d32d80d1bbee892",
+    "url": "/erp/static/js/5.8ba4bf5c.chunk.js"
+  },
+  {
+    "revision": "bff330fb270340c18896",
+    "url": "/erp/static/js/6.ff2fa6d3.chunk.js"
+  },
+  {
+    "revision": "e7f286e4acceb11ed3b9",
+    "url": "/erp/static/js/main.ef54e5c3.chunk.js"
+  },
+  {
+    "revision": "35907a8b2dd61e515825",
+    "url": "/erp/static/js/runtime-main.056a8753.js"
+  },
+  {
+    "revision": "74aaeeb9757d6af381f302c7800647f5",
+    "url": "/erp/static/media/bg1.74aaeeb9.jpg"
+  },
+  {
+    "revision": "b4e3503fd7ad4853a031739bfda5cead",
+    "url": "/erp/static/media/bg2.b4e3503f.jpg"
+  },
+  {
+    "revision": "e6ccd3dab6d6bf59ea6ff52e7baee4a2",
+    "url": "/erp/static/media/bg3.e6ccd3da.jpg"
+  },
+  {
+    "revision": "23d3b8d431b89b8aeb202b05e5d57423",
+    "url": "/erp/static/media/bg4.23d3b8d4.jpg"
+  },
+  {
+    "revision": "f6bb0b879c5ffec753783f2ae919a825",
+    "url": "/erp/static/media/guard-logo.f6bb0b87.png"
+  }
+]);
